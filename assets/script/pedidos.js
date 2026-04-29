71a01b494b76fef967560e4a601aae0569a8c99f
arhur-coelho-alves <Arthur56799@gmail.com>
2026-04-29 20:16:48 -0300

    Feito o pop-up adicionar produto

--- assets/script/pedidos.js
+++ assets/script/pedidos.js
@@ -15,46 +15,11 @@ const btnPipocaSimples = document.querySelector(".simples");
 const btnPipocaDoce = document.querySelector(".doce");
 
 let id = 0;
-let totalCompra = 0
+let totalCompra = 0;
 
 carrinho.addEventListener("click", aparecer);
 fecharCarrinho.addEventListener("click", fechar);
 
-btnSimples.addEventListener("click", () => {
-    adicionarEspeto("Espetinho Simples", "R$ 10,00");
-    totalCompra += 10;
-    atualizarTotal();
-});
-
-btnCompleto.addEventListener("click", () => {
-    adicionarEspeto("Espetinho Completo", "R$ 25,00");
-    totalCompra += 25;
-    atualizarTotal();
-});
-
-btnChurrosChocolate.addEventListener("click", () => {
-    adicionarChurros("Churros Chocolate", "R$ 8,00");
-    totalCompra += 8;
-    atualizarTotal();
-});
-
-btnChurrosDoceLeite.addEventListener("click", () => {
-    adicionarChurros("Churros Doce de Leite", "R$ 8,00");
-    totalCompra += 8;
-    atualizarTotal();
-});
-
-btnPipocaSimples.addEventListener("click", () => {
-    adicionarPipoca("Pipoca Simples", "R$ 10,00");
-    totalCompra += 10;
-    atualizarTotal();
-});
-
-btnPipocaDoce.addEventListener("click", () => {
-    adicionarPipoca("Pipoca de Doce ", "R$ 10,00");
-    totalCompra += 10;
-    atualizarTotal();
-});
 function aparecer() {
     aside.style.display = "flex";
     overlay.style.display = "block";
@@ -65,9 +30,85 @@ function fechar() {
     overlay.style.display = "none";
 }
 
-function removerPorId(id) {
-    const item = document.querySelector(`[data-id="${id}"]`);
-    if (item) item.remove();
+btnSimples.addEventListener("click", () => {
+    adicionarItem("Espetinho Simples", 10, "assets/images/espeto_carne.webp");
+    mensagem();
+});
+
+btnCompleto.addEventListener("click", () => {
+    adicionarItem("Espetinho Completo", 25, "assets/images/espeto_carne.webp");
+    mensagem();
+});
+
+btnChurrosChocolate.addEventListener("click", () => {
+    adicionarItem("Churros Chocolate", 8, "assets/images/churros.webp");
+    mensagem();
+});
+
+btnChurrosDoceLeite.addEventListener("click", () => {
+    adicionarItem("Churros Doce de Leite", 8, "assets/images/churros.webp");
+    mensagem();
+});
+
+btnPipocaSimples.addEventListener("click", () => {
+    adicionarItem("Pipoca Simples", 10, "assets/images/pipoca.jpg");
+    mensagem();
+});
+
+btnPipocaDoce.addEventListener("click", () => {
+    adicionarItem("Pipoca Doce", 10, "assets/images/pipoca.jpg");
+    mensagem();
+});
+
+function adicionarItem(nome, preco, imgSrc) {
+    const cartItem = document.createElement("div");
+    cartItem.classList.add("cart-item");
+
+    const id_atual = "produto-" + id;
+    id++;
+    cartItem.dataset.id = id_atual;
+    cartItem.dataset.preco = preco;
+
+    const header = document.createElement("div");
+    header.classList.add("cart-item-header");
+
+    const img = document.createElement("img");
+    img.src = imgSrc;
+
+    const title = document.createElement("p");
+    title.innerHTML = nome;
+
+    const btnRemove = criarBotaoRemover(id_atual);
+
+    const details = document.createElement("div");
+    details.classList.add("cart-item-details");
+
+    const spanPreco = document.createElement("span");
+    spanPreco.innerHTML = `R$ ${preco.toFixed(2)}`;
+
+    const controls = document.createElement("div");
+    controls.classList.add("cart-item-controls");
+
+    const btnMinus = document.createElement("button");
+    btnMinus.classList.add("btn-quantity");
+    btnMinus.innerHTML = "-";
+
+    const qtd = document.createElement("span");
+    qtd.innerHTML = "1";
+
+    const btnPlus = document.createElement("button");
+    btnPlus.classList.add("btn-quantity");
+    btnPlus.innerHTML = "+";
+
+    header.append(img, title, btnRemove);
+    controls.append(btnMinus, qtd, btnPlus);
+    details.append(spanPreco, controls);
+    cartItem.append(header, details);
+
+    cartContainer.append(cartItem);
+
+    totalCompra += preco;
+    atualizarTotal();
 }
 
 function criarBotaoRemover(id_atual) {
@@ -82,163 +123,53 @@ function criarBotaoRemover(id_atual) {
     return btn;
 }
 
-function adicionarEspeto(nome, preco) {
-    const cartItem = document.createElement("div");
-    cartItem.classList.add("cart-item");
+function removerPorId(id) {
+    const item = document.querySelector(`[data-id="${id}"]`);
 
-    const id_atual = "produto-" + id++;
-    cartItem.dataset.id = id_atual;
+    if (item) {
+        const preco = Number(item.dataset.preco);
 
-    const cartItemHeader = document.createElement("div");
-    cartItemHeader.classList.add("cart-item-header");
+        totalCompra -= preco;
+        item.remove();
 
-    const imgCart = document.createElement("img");
-    imgCart.src = "assets/images/espeto_carne.webp";
-
-    const cartP = document.createElement("p");
-    cartP.innerHTML = nome;
-
-    const cartRemove = criarBotaoRemover(id_atual);
-
-    const cartDetails = document.createElement("div");
-    cartDetails.classList.add("cart-item-details");
-
-    const cartSpan = document.createElement("span");
-    cartSpan.innerHTML = preco;
-
-    const cartControls = document.createElement("div");
-    cartControls.classList.add("cart-item-controls");
-
-    const cartReduce = document.createElement("button");
-    cartReduce.classList.add("btn-quantity");
-    cartReduce.innerHTML = "-";
-
-    const cartSpanQtd = document.createElement("span");
-    cartSpanQtd.innerHTML = "1";
-
-    const cartAdd = document.createElement("button");
-    cartAdd.classList.add("btn-quantity");
-    cartAdd.innerHTML = "+";
-
-    cartItemHeader.append(imgCart, cartP, cartRemove);
-    cartControls.append(cartReduce, cartSpanQtd, cartAdd);
-    cartDetails.append(cartSpan, cartControls);
-    cartItem.append(cartItemHeader, cartDetails);
-
-    cartContainer.append(cartItem);
-    total(total);
+        atualizarTotal();
+    }
 }
 
-function adicionarChurros(nome, preco) {
-    const cartItem = document.createElement("div");
-    cartItem.classList.add("cart-item");
-
-    const id_atual = "produto-" + id++;
-    cartItem.dataset.id = id_atual;
-
-    const cartItemHeader = document.createElement("div");
-    cartItemHeader.classList.add("cart-item-header");
-
-    const imgCart = document.createElement("img");
-    imgCart.src = "assets/images/churros.webp";
-
-    const cartP = document.createElement("p");
-    cartP.innerHTML = nome;
-
-    const cartRemove = criarBotaoRemover(id_atual);
-
-    const cartDetails = document.createElement("div");
-    cartDetails.classList.add("cart-item-details");
-
-    const cartSpan = document.createElement("span");
-    cartSpan.innerHTML = preco;
-
-    const cartControls = document.createElement("div");
-    cartControls.classList.add("cart-item-controls");
-
-    const cartReduce = document.createElement("button");
-    cartReduce.classList.add("btn-quantity");
-    cartReduce.innerHTML = "-";
-
-    const cartSpanQtd = document.createElement("span");
-    cartSpanQtd.innerHTML = "1";
-
-    const cartAdd = document.createElement("button");
-    cartAdd.classList.add("btn-quantity");
-    cartAdd.innerHTML = "+";
-
-    cartItemHeader.append(imgCart, cartP, cartRemove);
-    cartControls.append(cartReduce, cartSpanQtd, cartAdd);
-    cartDetails.append(cartSpan, cartControls);
-    cartItem.append(cartItemHeader, cartDetails);
-
-    cartContainer.append(cartItem);
-    total(total);
-}
-
-function adicionarPipoca(nome, preco) {
-    const cartItem = document.createElement("div");
-    cartItem.classList.add("cart-item");
-
-    const id_atual = "produto-" + id++;
-    cartItem.dataset.id = id_atual;
-
-    const cartItemHeader = document.createElement("div");
-    cartItemHeader.classList.add("cart-item-header");
-
-    const imgCart = document.createElement("img");
-    imgCart.src = "assets/images/pipoca.jpg";
-
-    const cartP = document.createElement("p");
-    cartP.innerHTML = nome;
-
-    const cartRemove = criarBotaoRemover(id_atual);
-
-    const cartDetails = document.createElement("div");
-    cartDetails.classList.add("cart-item-details");
-
-    const cartSpan = document.createElement("span");
-    cartSpan.innerHTML = preco;
-
-    const cartControls = document.createElement("div");
-    cartControls.classList.add("cart-item-controls");
-
-    const cartReduce = document.createElement("button");
-    cartReduce.classList.add("btn-quantity");
-    cartReduce.innerHTML = "-";
-
-    const cartSpanQtd = document.createElement("span");
-    cartSpanQtd.innerHTML = "1";
-
-    const cartAdd = document.createElement("button");
-    cartAdd.classList.add("btn-quantity");
-    cartAdd.innerHTML = "+";
-
-    cartItemHeader.append(imgCart, cartP, cartRemove);
-    cartControls.append(cartReduce, cartSpanQtd, cartAdd);
-    cartDetails.append(cartSpan, cartControls);
-    cartItem.append(cartItemHeader, cartDetails);
-
-    cartContainer.append(cartItem);
-    total(total);
-}
-function total(total){
-    const span_total = document.createElement("span")
-    span_total.innerHTML = "Total:";
-
-    const span_preco = document.createElement("span")
-    span_preco.innerHTML = `${total}`
-
-    cart_total.append(span_total, span_preco)
-}
 function atualizarTotal() {
     cart_total.innerHTML = "";
 
-    const span_total = document.createElement("span");
-    span_total.innerHTML = "Total: ";
+    const label = document.createElement("span");
+    label.innerHTML = "Total: ";
 
-    const span_preco = document.createElement("span");
-    span_preco.innerHTML = `R$ ${totalCompra.toFixed(2)}`;
+    const value = document.createElement("span");
+    value.innerHTML = `R$ ${totalCompra.toFixed(2)}`;
 
-    cart_total.append(span_total, span_preco);
+    cart_total.append(label, value);
+}
+function mensagem(){
+    const body = document.querySelector("body")
+
+    const body2 = document.createElement("div");
+    body2.classList.add("body2");
+    body2.style.display = "flex"
+    const divNotificacao = document.createElement("div")
+     divNotificacao.classList.add("notificacao");
+     body2.append(divNotificacao);
+     body.append(body2)
+
+
+     divNotificacao.style.display = "flex";
+
+     const paragrafo = document.createElement("p")
+     paragrafo.innerHTML = "Produto Adicionado";
+     divNotificacao.append(paragrafo)
+
+
+     setTimeout(() => {
+        body2.classList.add("fade-out");
+},1000);
+     setTimeout(() => {
+        body2.remove()
+}, 2000);
 }
